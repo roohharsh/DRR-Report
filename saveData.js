574bbfcee35923191548e50562b16a49478e1712
Harsh Singhal <harshparkar27@gmail.com>
2023-10-18 14:09:54 +0530

    ajax added and cancel button added

---
+++ saveData.js
@@ -0,0 +1,131 @@
+let currentId;
+
+window.addEventListener("load", function () {
+    // Initialize currentId from localStorage
+    currentId = parseInt(localStorage.getItem("currentId")) || 1;
+});
+
+// Function to save data and display it in the next row
+function saveData() {
+    const table = document.querySelector("table tbody");
+    const newRow = document.createElement("tr");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function () {
+        deleteRow(this);
+    });
+
+    const actionCell = document.createElement("td");
+    actionCell.appendChild(deleteButton);
+    newRow.appendChild(actionCell);
+
+    // Add an 'Id' cell with the current ID
+    const idCell = document.createElement("td");
+    idCell.textContent = currentId;
+    newRow.appendChild(idCell);
+    currentId++; // Increment the current ID
+    // Save the updated currentId to localStorage
+    localStorage.setItem("currentId", currentId);
+
+    // Extract input values
+    const inputs = document.querySelectorAll("input");
+    
+    let cellIndex = 1; // Start at 1 to account for the 'Id' cell
+    for (const input of inputs) {
+        if (input.type !== "button") { // Skip buttons
+            const cell = document.createElement("td");
+            cell.textContent = input.value;
+            newRow.appendChild(cell);
+            cellIndex++;
+        }
+    }
+
+    // Add a "Last Updated" cell with the current date and time
+    const lastUpdatedCell = document.createElement("td");
+    lastUpdatedCell.textContent = getCurrentDateTime();
+    newRow.appendChild(lastUpdatedCell);
+
+    // Insert the new row at the top of the table
+    if (table.firstChild) {
+        table.insertBefore(newRow, table.firstChild);
+    } else {
+        table.appendChild(newRow);
+    }
+
+    // Clear input fields
+    inputs.forEach((input) => {
+        if (input.type !== "button") {
+            input.value = '';
+        }
+    });
+
+    // Save the table data to localStorage
+    saveDataToLocalStorage();
+}
+
+
+
+
+// Function to delete a row
+function deleteRow(button) {
+    const row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+
+    // Save the table data to localStorage after deleting a row
+    saveDataToLocalStorage();
+}
+
+
+// Function to save the table data to localStorage
+function saveDataToLocalStorage() {
+    const tableRows = document.querySelectorAll("table tbody tr");
+    const savedData = [];
+
+    tableRows.forEach((row) => {
+        const cells = row.querySelectorAll("td");
+        const rowData = [];
+
+        cells.forEach((cell) => {
+            rowData.push(cell.textContent);
+        });
+
+        savedData.push(rowData);
+    });
+
+    localStorage.setItem("tableData", JSON.stringify(savedData));
+}
+
+
+// Function to clear input fields when "Cancel" button is clicked
+function cancel() {
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        if (input.type !== "button") {
+            input.value = '';
+        }
+    })
+}
+
+
+// Function to load saved data from localStorage and populate the table
+function loadSavedData() {
+    const table = document.querySelector("table tbody");
+    const savedData = JSON.parse(localStorage.getItem("tableData"));
+
+    if (savedData) {
+        savedData.forEach((rowData) => {
+            const newRow = document.createElement("tr");
+            rowData.forEach((cellData) => {
+                const cell = document.createElement("td");
+                cell.textContent = cellData;
+                newRow.appendChild(cell);
+            });
+            table.appendChild(newRow);
+        });
+    }
+}
+
+
+// Call the function to load saved data when the page loads
+window.addEventListener("load", loadSavedData);
--- trial.js
+++ trial.js
@@ -97,6 +97,136 @@ function getCurrentDateTime() {
     return formattedDate;
 }
 
+// Function to clear input fields when "Cancel" button is clicked
+function cancel() {
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        if (input.type !== "button") {
+            input.value = '';
+        }
+    })
+}
+
+
+
+
+// Function to save data using AJAX
+function saveData() {
+    // Get the values from the input fields
+    const startDate = document.getElementById("start-date").value;
+    const endDate = document.getElementById("end-date").value;
+    const monthYear = document.querySelector(".month-input").value;
+    const excludedDate = document.getElementById("excluded-date").value;
+    const days = document.querySelector(".days-input").value;
+    const leadCount = document.getElementById("lead-count").value;
+    const expectedLeadCount = document.querySelector(".expected-lead-count-input").value;
+
+    // Create a JavaScript object to hold the data we want to send
+    const data = {
+        startDate: startDate,
+        endDate: endDate,
+        monthYear: monthYear,
+        excludedDate: excludedDate,
+        days: days,
+        leadCount: leadCount,
+        expectedLeadCount: expectedLeadCount,
+        lastUpdated: getCurrentDateTime(),
+    };
+
+    // Create a new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+
+    // Define the AJAX request method, URL
+    xhr.open("POST", "https://reqres.in/api/users");
+
+    // Set the request header for sending data in JSON format
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // Define the callback function to handle the AJAX response
+    xhr.onload = function () {
+        if (xhr.status === 201) {
+            // Request was successful, we can add the new row to the UI
+            const responseData = JSON.parse(xhr.responseText);
+            addRowToTable(responseData);
+
+            // Clear input fields after successful submission
+            clearInputFields();
+        } else {
+            // Handle errors or display an error message
+            console.error("Error saving data:", xhr.status, xhr.statusText);
+        }
+    };
+
+    // Convert the data object to a JSON string
+    const jsonData = JSON.stringify(data);
+
+    // Send the JSON data in the request body
+    xhr.send(jsonData);
+}
+
+// Function to add a new row to the table in the UI
+function addRowToTable(data) {
+    const tableBody = document.querySelector(".drr-table tbody");
+    const newRow = tableBody.insertRow();
+
+    newRow.innerHTML = `
+        <td>${data.id}</td>
+        <td>${data.startDate}</td>
+        <td>${data.endDate}</td>
+        <td>${data.monthYear}</td>
+        <td>${data.excludedDate}</td>
+        <td>${data.days}</td>
+        <td>${data.leadCount}</td>
+        <td>${data.expectedLeadCount}</td>
+        <td>${data.lastUpdated}</td>
+    `;
+}
+
+// Function to clear input fields
+function clearInputFields() {
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        if (input.type !== "button") {
+            input.value = '';
+        }
+    });
+}
+
+// Function to load data using AJAX
+function loadData() {
+    // Create a new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+
+    // Define the AJAX request method, URL
+    xhr.open("GET", "https://reqres.in/api/users");
+
+    // Define the callback function to handle the AJAX response
+    xhr.onload = function () {
+        if (xhr.status === 201) {
+            // Request was successful, we can add the rows to the UI
+            const responseData = JSON.parse(xhr.responseText);
+            responseData.data.forEach((item) => {
+                addRowToTable(item);3
+
+            });
+        } else {
+            // Handle errors or display an error message
+            console.error("Error loading data:", xhr.status, xhr.statusText);
+        }
+    };
+
+    // Send the GET request
+    xhr.send();
+}
+
+// Add an event listener to load data when the page is loaded
+window.addEventListener("load", function () {
+    loadData();
+});
+
+
+
+
 // Add event listeners to your input fields
 document.querySelector(".month-input").addEventListener("click", function () {
     fillMonthYear(this);
